a1493ab27d4a4b5eef37fc930a600e9fcb746f7f
ToJaNikodem <nikodembuczkowski05@gmail.com>
2026-04-19 23:19:56 +0200

    fix: tailwind working

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+document.documentElement.classList.add('dark')
+
 // biome-ignore lint/style/noNonNullAssertion: Root is always defined
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
